a8ab6d545edbb6ead1fdc408de5b78a038d0a441
Frank Gutierrez <fgutiefe@emeal.nttdata.com>
2026-05-13 10:14:50 -0500

    feat: Implement mapper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,7 @@
 import { View, Text } from 'react-native'
 import '../global.css';
-import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 
 const RootLayout = () => {
-  nowPlayingAction();
   return (
     <View>
       <Text>RootLayout</Text>
--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,11 +1,14 @@
 import { movieApi } from "@/core/api/movie-api";
 import { MovieDBResponse } from "@/infrastructure/interface/moviedb-response";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
 
 export const nowPlayingAction = async() => {
     try {
         const { data } = await movieApi.get<MovieDBResponse>('/now_playing')
 
-        return [];
+        const movies = data.results.map((movie) => MovieMapper.fromMovieDBtoMovie(movie))
+
+        return movies;
 
     } catch(error) {
         console.log(error)
