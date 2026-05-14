b608d6708ec18570fe5c95e385adda4b87f26700
Frank Gutierrez <fgutiefe@emeal.nttdata.com>
2026-05-14 00:10:52 -0500

    feat: Implement TansTack Query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,28 @@
 import { View, Text } from 'react-native'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import '../global.css';
+import { Stack } from 'expo-router';
+
+
+// Create a client
+const queryClient = new QueryClient()
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <Stack 
+        screenOptions={{
+          headerShown: false
+        }}
+      />
+    </QueryClientProvider>
   )
 }
 
---
+++ app/home/index.tsx
@@ -0,0 +1,20 @@
+import { useMovies } from "@/presentation/hooks/useMovies";
+import { View, Text } from "react-native"
+
+const HomeScreen = () => {
+
+    const { nowPlayingQuery } = useMovies();
+
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+            <Text>
+                {
+                    JSON.stringify(nowPlayingQuery.data, null , 2)
+                }
+            </Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
