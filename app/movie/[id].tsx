2b0ee7386d4a42b232792393f97440c96f9d98ed
Frank Gutierrez <fgutiefe@emeal.nttdata.com>
2026-05-17 17:56:10 -0500

    feat: Implement MovieHeader Hook

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,3 +1,4 @@
+import MovieHeader from "@/presentation/components/movie/MovieHeader";
 import { useMovie } from "@/presentation/hooks/useMovie";
 import { useLocalSearchParams } from "expo-router";
 import { ActivityIndicator, ScrollView, Text, View } from "react-native";
@@ -7,7 +8,7 @@ const MovieScreen = () => {
 
     const { movieQuery } = useMovie(+id);
 
-    if(movieQuery.isLoading){
+    if(movieQuery.isLoading || !movieQuery.data){
         return (
             <View className="flex flex-1 justify-center items-center">
                 <Text className="mb-4" >Espere por favor</Text>
@@ -18,9 +19,11 @@ const MovieScreen = () => {
 
     return (
         <ScrollView>
-            <Text>
-                { movieQuery.data?.title ?? "Sin titulo"}
-            </Text>
+            <MovieHeader
+                originalTitle={ movieQuery.data.originalTitle }
+                poster={ movieQuery.data.poster }
+                title={ movieQuery.data.title }
+            />
         </ScrollView>
     );
 };
